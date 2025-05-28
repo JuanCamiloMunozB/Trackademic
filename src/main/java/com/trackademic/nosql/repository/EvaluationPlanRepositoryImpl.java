21d16dac6345a89105aa7685b28c70eb09936f58
Andres Chamorro <chamorroricardo29@gmail.com>
2025-05-27 22:29:07 -0500

    feat: add correction in evaluationPlanReposityImpl

--- src/main/java/com/trackademic/nosql/repository/EvaluationPlanRepositoryImpl.java
+++ src/main/java/com/trackademic/nosql/repository/EvaluationPlanRepositoryImpl.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.stereotype.Repository; // Optional but good practice
 
 import java.util.ArrayList;
@@ -16,18 +19,17 @@ import java.util.List;
 public class EvaluationPlanRepositoryImpl implements EvaluationPlanRepositoryCustom {
 
     @Autowired
-    private  MongoTemplate mongoTemplate;
+    private MongoTemplate mongoTemplate;
 
-  
     @Override
-    public List<EvaluationPlan> findByDynamicCriteria(
+    public Page<EvaluationPlan> findByDynamicCriteria(
             ObjectId studentId,
             String subjectCode,
             String subjectName,
             String groupId,
             String professor,
-            String semester
-    ) {
+            String semester,
+            Pageable pageable) {
         Query query = new Query();
         List<Criteria> criteriaList = new ArrayList<>();
 
@@ -38,23 +40,26 @@ public class EvaluationPlanRepositoryImpl implements EvaluationPlanRepositoryCus
             criteriaList.add(Criteria.where("subject_code").is(subjectCode));
         }
         if (subjectName != null && !subjectName.isEmpty()) {
-             criteriaList.add(Criteria.where("subject_name").is(subjectName));
+            criteriaList.add(Criteria.where("subject_name").is(subjectName));
         }
         if (groupId != null && !groupId.isEmpty()) {
             criteriaList.add(Criteria.where("group_id").is(groupId));
         }
         if (professor != null && !professor.isEmpty()) {
-            criteriaList.add(Criteria.where("professor").regex(professor, "i")); 
+            criteriaList.add(Criteria.where("professor").regex(professor, "i"));
         }
         if (semester != null && !semester.isEmpty()) {
             criteriaList.add(Criteria.where("semester").is(semester));
         }
 
         if (!criteriaList.isEmpty()) {
-
             query.addCriteria(new Criteria().andOperator(criteriaList.toArray(new Criteria[0])));
         }
 
-        return mongoTemplate.find(query, EvaluationPlan.class);
+        long total = mongoTemplate.count(query, EvaluationPlan.class);
+        query.with(pageable);
+        List<EvaluationPlan> content = mongoTemplate.find(query, EvaluationPlan.class);
+
+        return new PageImpl<>(content, pageable, total);
     }
 }
